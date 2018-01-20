78b9d15df022de44f433d56d19c0bbe42bb6ab3c
hejny <me@pavolhejny.com>
2018-01-20 07:33:59 +0100

    + CORS

--- src/httpApiRouter.js
+++ src/httpApiRouter.js
@@ -6,6 +6,9 @@ var getContent = require('./getContent');
 const router = express.Router();
 
 router.use('/*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
